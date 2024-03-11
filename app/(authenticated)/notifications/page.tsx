840e2bc94848c76a2b8af80f1038c283de0a2083
Kapil Bastola <kapilmuni209@gmail.com>
2024-03-11 23:48:15 +0530

    Feat: Notifications page first phase implementation

--- app/(authenticated)/notifications/page.tsx
+++ app/(authenticated)/notifications/page.tsx
@@ -1,12 +1,69 @@
+'use client'
+
+import { Post } from '../../../components/pages/home/Posts/Post'
+import { PostProps } from '../../../components/pages/home/Posts/types'
+
 import styles from './page.module.sass'
+
+import { useGetList } from '@packages/core/hooks'
+import { useEffect, useState } from 'react'
 
 const Notifications = () => {
 
+
+	const [feeds, setFeeds] = useState<PostProps[]>()
+
+	const { loadData } = useGetList({
+		resource: 'posts',
+		sort: [
+			{
+				field: '$createdAt',
+				order: 'desc',
+			},
+		],
+	})
+
+	useEffect(() => {
+
+		let mounted = true;
+
+		if(mounted){
+			loadData({
+				resource: process.env.NEXT_PUBLIC_CHAT_COLLECTION_ID as string,
+				sort: [
+					{
+						field: '$createdAt',
+						order: 'desc',
+					},
+				],
+			}).then(
+				(response) => {
+
+					if(response?.total){
+						setFeeds(response?.data as unknown as PostProps[])
+					}else{
+						setFeeds([])
+					}
+				},
+				(error) => {
+					console.log(error)
+				}
+			)
+		}
+
+
+		return () => {
+			mounted = false
+		}
+	}, [])
+
+
 	return (
-		<main className={styles.main}>
-			Notifications
-		</main>
+		<div className={styles.feeds}>
+			{feeds?.map((feed : PostProps) => <Post key={feed.id} {...feed} />)}
+		</div>
 	)
 }
 
-export default Notifications
+
+export default Notifications;
